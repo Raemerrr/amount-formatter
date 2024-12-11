e19cb44f90304dc3c2932018e968e95f94537280
raemin.kang <raemin.kang@navercorp.com>
2024-12-11 22:42:13 +0900

    배포 스크립트 수정 v0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -69,7 +69,7 @@ publishing {
 
 signing {
     useInMemoryPgpKeys(
-            System.getenv("SIGNING_KEY"),
+            System.getenv("GPG_PRIVATE_KEY"),
             System.getenv("GPG_PASSPHRASE")
     )
     sign(publishing.publications["mavenJava"])
