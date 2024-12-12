d575e67e625dbe324ab804a0c49f80e42e9de32f
raemin.kang <raemin.kang@navercorp.com>
2024-12-12 23:18:35 +0900

    ci: 배포 스크립트 업로드

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,81 +1,56 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     id("java")
-    id("maven-publish")
+    id("com.vanniktech.maven.publish") version "0.28.0"
     id("signing")
 }
 
-group = "io.github.raemerrr"
-version = "0.0.1"
-
-java {
-    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
-    withSourcesJar()
-    withJavadocJar()
-}
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
-
-            pom {
-                name.set("Amount Formatter")
-                description.set("A simple library to format amounts")
-                url.set("https://github.com/Raemerrr/amount-formatter")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                        distribution.set("repo")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("Raemerrr")
-                        name.set("Raemin Kang")
-                        email.set("foals18@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/Raemerrr/amount-formatter.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:Raemerrr/amount-formatter.git")
-                    url.set("https://github.com/Raemerrr/amount-formatter")
-                }
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-            credentials {
-                username = project.findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
-                password = project.findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
-            }
-        }
-    }
-}
+rootProject.description = "A simple library to format amounts"
 
 signing {
     useInMemoryPgpKeys(
             System.getenv("GPG_PRIVATE_KEY"),
             System.getenv("GPG_PASSPHRASE")
     )
-    sign(publishing.publications["mavenJava"])
+    sign(publishing.publications)
 }
 
-tasks.test {
-    useJUnitPlatform()
+mavenPublishing {
+    coordinates(
+            groupId = "io.github.raemerrr",
+            artifactId = "amount-formatter",
+            version = System.getenv("VERSION")
+    )
+
+    pom {
+        name.set(project.rootProject.name)
+        description.set(project.description)
+        inceptionYear.set("2024")
+        url.set("https://github.com/Raemerrr/amount-formatter")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/Raemerrr/amount-formatter/blob/main/LICENSE")
+                distribution.set("repo")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("Raemerrr")
+                name.set("Raemin Kang")
+                email.set("foals18@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:git://github.com/Raemerrr/amount-formatter.git")
+            developerConnection.set("scm:git:ssh://git@github.com:Raemerrr/amount-formatter.git")
+            url.set("https://github.com/Raemerrr/amount-formatter")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 }
