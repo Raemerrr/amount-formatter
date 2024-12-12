f47b175e1a5ad7516a35c692a77d5cb9fb7d98b5
raemin.kang <raemin.kang@navercorp.com>
2024-12-12 19:45:53 +0900

    배포 스크립트 수정 v0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     id("java")
-    id("com.vanniktech.maven.publish") version "0.28.0"
+    id("maven-publish")
     id("signing")
 }
 
 group = "io.github.raemerrr"
 version = System.getenv("VERSION")
+rootProject.description = "A simple library to format amounts"
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    withJavadocJar()
     withSourcesJar()
 }
 
@@ -19,47 +19,15 @@ repositories {
 }
 
 dependencies {
-
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
 }
 
-mavenPublishing {
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
-
-    signAllPublications()
-
-    coordinates("io.github.raemerrr", "amount-formatter", System.getenv("VERSION"))
-
-    pom {
-        name.set("Amount Formatter")
-        description.set("A simple library to format amounts")
-        url.set("https://github.com/Raemerrr/amount-formatter")
-
-        licenses {
-            license {
-                name.set("MIT License")
-                url.set("https://opensource.org/licenses/MIT")
-                distribution.set("repo")
-            }
-        }
-
-        developers {
-            developer {
-                id.set("Raemerrr")
-                name.set("Raemin Kang")
-                email.set("foals18@gmail.com")
-            }
-        }
-
-        scm {
-            connection.set("scm:git:git://github.com/Raemerrr/amount-formatter.git")
-            developerConnection.set("scm:git:ssh://git@github.com:Raemerrr/amount-formatter.git")
-            url.set("https://github.com/Raemerrr/amount-formatter")
-        }
-    }
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 signing {
+    isRequired = System.getenv("OSSRH_USERNAME") != null
     useInMemoryPgpKeys(
             System.getenv("GPG_PRIVATE_KEY"),
             System.getenv("GPG_PASSPHRASE")
@@ -69,4 +37,54 @@ signing {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            from(components["java"])
+
+            pom {
+
+                name.set("AmountFormatter")
+                description.set(project.description)
+                url.set("https://github.com/Raemerrr/amount-formatter")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/Raemerrr/amount-formatter/blob/main/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("Raemerrr")
+                        name.set("Raemin Kang")
+                        email.set("foals18@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Raemerrr/amount-formatter.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:Raemerrr/amount-formatter.git")
+                    url.set("https://github.com/Raemerrr/amount-formatter")
+                }
+
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
+            }
+        }
+    }
 }
