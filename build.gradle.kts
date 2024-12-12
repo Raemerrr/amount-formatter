ba44a77c116ad1910bb2bfa89074d004db76ae26
raemin.kang <raemin.kang@navercorp.com>
2024-12-12 20:12:27 +0900

    배포 스크립트 수정 v0.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,30 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     id("java")
-    id("com.vanniktech.maven.publish") version "0.28.0"
+    id("maven-publish")
     id("signing")
 }
 
+group = "io.github.raemerrr"
+version = System.getenv("VERSION")
 rootProject.description = "A simple library to format amounts"
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    withJavadocJar()
+    withSourcesJar()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
 
 signing {
     isRequired = System.getenv("OSSRH_USERNAME") != null
@@ -17,53 +35,55 @@ signing {
     sign(publishing.publications)
 }
 
-mavenPublishing {
-    coordinates(
-            groupId = "io.github.raemerrr",
-            artifactId = "amount-formatter",
-            version = System.getenv("VERSION")
-    )
-
-    pom {
-        name.set(project.rootProject.name)
-        description.set(project.description)
-        inceptionYear.set("2024")
-        url.set("https://github.com/Raemerrr/amount-formatter")
-
-        licenses {
-            license {
-                name.set("MIT License")
-                url.set("https://github.com/Raemerrr/amount-formatter/blob/main/LICENSE")
-                distribution.set("repo")
-            }
-        }
-
-        developers {
-            developer {
-                id.set("Raemerrr")
-                name.set("Raemin Kang")
-                email.set("foals18@gmail.com")
-            }
-        }
-
-        scm {
-            connection.set("scm:git:git://github.com/Raemerrr/amount-formatter.git")
-            developerConnection.set("scm:git:ssh://git@github.com:Raemerrr/amount-formatter.git")
-            url.set("https://github.com/Raemerrr/amount-formatter")
-        }
-    }
-
-    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+tasks.test {
+    useJUnitPlatform()
 }
 
 publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+
+            from(components["java"])
+
+            pom {
+
+                name.set("AmountFormatter")
+                description.set(project.description)
+                url.set("https://github.com/Raemerrr/amount-formatter")
+
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/Raemerrr/amount-formatter/blob/main/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("Raemerrr")
+                        name.set("Raemin Kang")
+                        email.set("foals18@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Raemerrr/amount-formatter.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:Raemerrr/amount-formatter.git")
+                    url.set("https://github.com/Raemerrr/amount-formatter")
+                }
+
+            }
+        }
+    }
+
     repositories {
         maven {
             name = "MavenCentral"
             url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
             credentials {
-                username = System.getenv("OSSRH_USERNAME") ?: throw GradleException("MAVEN_CENTRAL_USERNAME is not set")
-                password = System.getenv("OSSRH_PASSWORD") ?: throw GradleException("MAVEN_CENTRAL_PASSWORD is not set")
+                username = findProperty("ossrhUsername") as String? ?: System.getenv("OSSRH_USERNAME")
+                password = findProperty("ossrhPassword") as String? ?: System.getenv("OSSRH_PASSWORD")
             }
         }
     }
